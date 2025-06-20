cc7656e74b24c9f6b16ed167077f4a30bdb81be3
Manjunath-04-SR <manjusr4123@gmail.com>
2025-06-20 15:19:43 +0530

    Add files via upload

---
+++ week_1 Hands_0n_Project Assignment/Factory Method Pattern/Client/FactoryMethodDemo.java
@@ -0,0 +1,20 @@
+package client;
+
+import factories.*;
+import documents.Document;
+
+public class FactoryMethodDemo {
+    public static void main(String[] args) {
+        DocumentFactory wordFactory = new WordDocumentFactory();
+        Document wordDoc = wordFactory.createDocument();
+        wordDoc.open();
+
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        Document pdfDoc = pdfFactory.createDocument();
+        pdfDoc.open();
+
+        DocumentFactory excelFactory = new ExcelDocumentFactory();
+        Document excelDoc = excelFactory.createDocument();
+        excelDoc.open();
+    }
+}
